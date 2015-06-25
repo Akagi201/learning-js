31818debe23337d27d1bf0d3e64a4fc53394386c
Akagi201 <akagi201@gmail.com>
2015-06-25 14:42:54 +0800

    add testArray

---
+++ nodejs/misc/testArray.js
@@ -0,0 +1,8 @@
+
+var arr = ['this is the first element', 'this is the second element'];
+
+console.log(arr[0]);    // logs 'this is the first element'
+console.log(arr[0]);    // logs 'this is the second element'
+console.log(arr[arr.length - 1]);   // logs 'this is the second element'
+
+console.log(arr[-1]); // undefined

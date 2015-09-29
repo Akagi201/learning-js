103e731b09a416c0ece7ea6c208c80cecd493647
Akagi201 <akagi201@gmail.com>
2015-09-29 16:16:07 +0800

    node: add cmd tests

---
+++ nodejs/shelljs_global.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+require('shelljs/global');
+
+if (!which('git')) {
+    echo('Sorry, this script requires git');
+    exit(1);
+}
+
+mkdir('-p', 'out/Release');
+cp('-R', 'stuff/*', 'out/Release');
+
+cd('lib');
+ls('*.js').forEach(function(file) {
+    sed('-i', 'v0.1.2', 'v0.1.2', file);
+    sed('-i', /.*REPLACE_LINE_WITH_MACRO.*\n/, cat('macro.js'), file);
+});
+
+cd('..');
+
+if (exec('git commit -am "Auto-commit"').code != 0) {
+    echo('Error: Git commit failed');
+    exit(1);
+}

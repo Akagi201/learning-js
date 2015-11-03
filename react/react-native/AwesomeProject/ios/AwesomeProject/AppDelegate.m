aa54cea50af60133a98ccc4f3c86bff3185a29c0
Akagi201 <akagi201@gmail.com>
2015-11-03 22:17:20 +0800

    react-native: add helloworld

--- react/react-native/AwesomeProject/ios/AwesomeProject/AppDelegate.m
+++ react/react-native/AwesomeProject/ios/AwesomeProject/AppDelegate.m
@@ -31,7 +31,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.12.3:8081/index.ios.bundle"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -46,7 +46,7 @@
 //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"HelloWorld"
+                                                      moduleName:@"AwesomeProject"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
 

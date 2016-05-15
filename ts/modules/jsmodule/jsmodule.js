c2b67ea59025afa0f92a4acb078e39831f40b844
Akagi201 <akagi201@gmail.com>
2016-05-15 15:46:06 +0800

    ts: add module

---
+++ ts/modules/jsmodule/jsmodule.js
@@ -0,0 +1,20 @@
+var jkxyModule = function(vip) {
+  // 声明私有成员
+  var Yvip = document.getElementById(vip);
+  YQ = "123456789";
+  return {
+    // 公开的成员
+    add: function(t) {
+      if (t >= 12) {
+        var Yv = t;
+        Yvip.innerHTML="年费:" + Yv + "-- QQ群:" + YQ;
+      } else {
+        var Mv = t;
+        Yvip.innerHTML="月费:" + Mv;
+      }
+    }
+  }
+}
+
+var jm = new jkxyModule("vip");
+jm.add(12);
